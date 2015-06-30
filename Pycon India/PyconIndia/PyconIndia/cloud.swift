a96a20ec5a55df380c52c2d0655b3171e3a2b569
Rishi Mukherjee <rishi@gopassport.com>
2015-06-30 09:55:25 +0530

    Added dependencied

---
+++ Pycon India/PyconIndia/PyconIndia/cloud.swift
@@ -0,0 +1,20 @@
+//
+//  cloud.swift
+//  PyconIndia
+//
+//  Created by Rishi Mukherjee on 30/06/15.
+//  Copyright (c) 2015 Rishi Mukherjee. All rights reserved.
+//
+
+import Foundation
+
+// All API related code here.
+class Cloud {
+
+    let BASE_URL = "https://junctiondemo.herokuapp.com/a-porro-conference/schedule/dummy_schedule/"
+
+    func apiRequest(path: String, method: String = "GET") {
+
+    }
+
+}
